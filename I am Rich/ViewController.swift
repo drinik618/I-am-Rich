7dc25c918bc1f1d5e263027b6fe9a5c770513a01
drinik618 <rinik.dylan18@gmail.com>
2021-07-07 08:39:43 -0400

    I am Rich Application

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dylan Rinik on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
